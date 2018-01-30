35c7088c6eee840bf14e7a0acf0f7026b3c95a30
Chr15t1an <ccampbell@sapphirebd.com>
2018-01-30 14:56:42 -0500

    Spelling update.
    Spelling update.

--- resources/views/public/thank_you.blade.php
+++ resources/views/public/thank_you.blade.php
@@ -1,5 +1,5 @@
 @extends('layout')
 
 @section('content')
-<h1 class="text-center">Thank you for subimitting your info </h2>
+<h1 class="text-center">Thank you for submitting your info. </h2>
 @endsection
